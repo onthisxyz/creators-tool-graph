b67186a4b25debf5d24503824cf13d3835e19777
Danil Dovgal <91081974+qweblessed@users.noreply.github.com>
2024-03-02 05:01:07 -0500

    feat: creators tool graph

--- src/contract.ts
+++ src/contract.ts
@@ -4,50 +4,19 @@ import {
   OwnershipTransferred as OwnershipTransferredEvent
 } from "../generated/Contract/Contract"
 import {
-  Initialized,
-  NewShortcutCreated,
-  OwnershipTransferred
+  Shortcut,  
 } from "../generated/schema"
 
-export function handleInitialized(event: InitializedEvent): void {
-  let entity = new Initialized(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.version = event.params.version
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
 export function handleNewShortcutCreated(event: NewShortcutCreatedEvent): void {
-  let entity = new NewShortcutCreated(
+  let entity = new Shortcut(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
-  entity.params_router = event.params.params.router
-  entity.params_tokenOut = event.params.params.tokenOut
-  entity.params_chainId = event.params.params.chainId
-  entity.params_fee = event.params.params.fee
-  entity.params_dex = event.params.params.dex
+  entity.router = event.params.params.router
+  entity.tokenOut = event.params.params.tokenOut
+  entity.chainId = event.params.params.chainId
+  entity.fee = event.params.params.fee
+  entity.dex = event.params.params.dex
   entity.shortcut = event.params.shortcut
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleOwnershipTransferred(
-  event: OwnershipTransferredEvent
-): void {
-  let entity = new OwnershipTransferred(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
 
   entity.blockNumber = event.block.number
   entity.blockTimestamp = event.block.timestamp
